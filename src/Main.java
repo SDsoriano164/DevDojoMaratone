4d2fedba519a1d46de73100ff7e483d3d554eb65
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-01-15 20:52:10 -0300

    your msg

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // int - float - double - long - byte - short - boolean - char
+
+        int numInteiro = 1000;
+        float numFloat = 4541.3f;
+        double numDouble = 4000.21D;
+        long numLong = 21045847125L;
+        byte numByte = -128;
+        short numShort=421;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char Ischar = 65;
+        System.out.println(numInteiro);
+        System.out.println(numFloat);
+        System.out.println(numDouble);
+        System.out.println(numLong);
+        System.out.println(numShort);
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+        System.out.println((Ischar));
+
+    }
+}
