ec09cee20ca5d977a621b545de7dd8c8d5f766d1
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-07 20:37:04 -0300

    estudando mais um pouco sobre classes

---
+++ src/DevDojoMaratone/Javacore/Aintroducaoclasses/domain/ContasDaCasa.java
@@ -0,0 +1,13 @@
+package DevDojoMaratone.Javacore.Aintroducaoclasses.domain;
+
+public class ContasDaCasa {
+    public int casa;
+    public int energia;
+    public int condominio;
+    public int internet;
+    public int psicologo;
+    public int Universidade;
+    public int cartões;
+
+
+}
---
+++ src/DevDojoMaratone/Javacore/Aintroducaoclasses/test/ContasCasaTeste.java
@@ -0,0 +1,18 @@
+package DevDojoMaratone.Javacore.Aintroducaoclasses.test;
+
+import DevDojoMaratone.Javacore.Aintroducaoclasses.domain.ContasDaCasa;
+
+public class ContasCasaTeste {
+    public static void main(String[] args) {
+        ContasDaCasa conta = new ContasDaCasa();
+
+        conta.condominio = 300;
+        conta.casa = 580;
+        conta.internet
+        conta.
+        conta.
+        conta.
+        conta.
+        conta.
+    }
+}
