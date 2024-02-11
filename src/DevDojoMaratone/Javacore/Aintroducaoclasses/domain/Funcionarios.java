8200249255d15d219f4e855feecf4a38e8194fed
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-11 20:14:58 -0300

    Chamando metodo de calculo dentro do metetodo para imprimir as informações, podem ser  feita dessas duas formas

--- src/DevDojoMaratone/Javacore/Aintroducaoclasses/domain/Funcionarios.java
+++ src/DevDojoMaratone/Javacore/Aintroducaoclasses/domain/Funcionarios.java
@@ -1,26 +1,45 @@
 package DevDojoMaratone.Javacore.Aintroducaoclasses.domain;
 
 import java.text.DecimalFormat;
+
 public class Funcionarios {
+    // Atributos da classe
     public String nome;
     public int idade;
     public double[] salario;
 
+    // DecimalFormat para formatar os salários
     DecimalFormat df = new DecimalFormat("R$ #,##0.00"); // Define o formato desejado
-    public void imprime(){
-        System.out.println("Nome : " + this.nome);
-        System.out.println("Idade : " + idade);
-        for (double salarios : salario){
-            System.out.print("Salario : " + df.format(salarios) + "\n ");
-        }
-    }
-    public void imprimeMedia(){
-        double media = 0;
-        for (double salarios:salario){
-            media += salarios;
 
+    // Método para imprimir os atributos da classe
+    public void imprime() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + idade);
+
+        // Mostrando os salários usando for-each
+        for (double salarios : salario) {
+            System.out.print("Salário: " + df.format(salarios) + "\n ");
         }
+
+        // Chamando o método imprimeMedia para imprimir a média salarial
+        imprimeMedia();
+
+        System.out.println("------------------------");
+    }
+
+    // Método para calcular e imprimir a média salarial
+    public void imprimeMedia() {
+        double media = 0;
+
+        // Calculando a soma dos salários
+        for (double salarios : salario) {
+            media += salarios;
+        }
+
+        // Calculando a média
         media /= salario.length;
-        System.out.println("Media salarial : " + df.format(media));
+
+        // Imprimindo a média formatada
+        System.out.println("Média salarial: " + df.format(media));
     }
 }
