e5f52f70117abf479fe36b709c9118adb0be1a19
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-12 21:06:22 -0300

    Criando variavel de classe e mostrando como remover o acesso a ela, crimodificando os acessos dos atributos criando metodos de acesso

--- src/DevDojoMaratone/Javacore/Aintroducaoclasses/domain/Funcionarios.java
+++ src/DevDojoMaratone/Javacore/Aintroducaoclasses/domain/Funcionarios.java
@@ -4,9 +4,10 @@ import java.text.DecimalFormat;
 
 public class Funcionarios {
     // Atributos da classe
-    public String nome;
-    public int idade;
-    public double[] salario;
+    private String nome;
+    private int idade;
+    private double media;
+    private double[] salario;
 
     // DecimalFormat para formatar os salários
     DecimalFormat df = new DecimalFormat("R$ #,##0.00"); // Define o formato desejado
@@ -18,22 +19,17 @@ public class Funcionarios {
         if (salario == null) {
             return;
         }
-
             for (double salarios : salario) {
                 System.out.print("Salário: " + df.format(salarios) + "\n ");
             }
             imprimeMedia();
             System.out.println("------------------------");
-
-
-
     }
 
 
     // Método para calcular e imprimir a média salarial
     public void imprimeMedia() {
 
-            double media = 0;
             if (salario == null){
                 return;
             }
@@ -47,5 +43,40 @@ public class Funcionarios {
             media /= salario.length;
             System.out.println("Média salarial: " + df.format(media));
         }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+     // ao deletar esta linha, não terá mais como modificar o valor da variavel
+    //public void setMedia(double media) {
+       // this.media = media;
+    //}
+
+    public void setSalario(double[] salario) {
+        this.salario = salario;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public double[] getSalario() {
+        return salario;
+    }
+
+}
 
