ed9394a160644901abfedb695b1e333177d4e952
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-12 21:07:46 -0300

    Atribuindo valores ao setMedia para mostrar como remover o acesso ao método para setar valores e modificar a variavel de classe

--- src/DevDojoMaratone/Javacore/Aintroducaoclasses/test/FuncionariosTeste01.java
+++ src/DevDojoMaratone/Javacore/Aintroducaoclasses/test/FuncionariosTeste01.java
@@ -10,27 +10,37 @@ public class FuncionariosTeste01 {
         Funcionarios funcionarios2 = new Funcionarios();
         Funcionarios funcionarios3 = new Funcionarios();
 
+        //modificando media
+
+        funcionarios1.setMedia(2000);
+
 
         // funcionario 1
-        funcionarios1.nome = "Pedro Soriano";
-        funcionarios1.idade = 24;
+        funcionarios1.setNome("Pedro Soriano");
+        funcionarios1.setIdade(24);
+        funcionarios1.setSalario(new double[] {3124,3123,4142});
 
         //funcionarios1.salario = new double[]{7000,1000,33};
 
         // funcionario 2
-        funcionarios2.nome = "Caroline Soriano";
-        funcionarios2.idade = 24;
-        funcionarios2.salario = new double[]{9000,2231,545};
+        funcionarios2.setNome("Caroline Soriano");
+        funcionarios2.setIdade(23);
+        funcionarios2.setSalario(new double[]{9000,2231,545});
 
         // funcionario 3
-        funcionarios3.nome = "Eduardo Soriano";
-        funcionarios3.idade = 24;
 
-        funcionarios3.salario = new double[]{1500,1412,3455};
+        funcionarios3.setNome("Eduardo Soriano");
+        funcionarios3.setIdade(24);
+        funcionarios3.setSalario(new double[]{1500,1412,3455});
 
         funcionarios1.imprime();
         funcionarios2.imprime();
         funcionarios3.imprime();
+
+
+        System.out.println("Media alterada ");
+
+        System.out.println(funcionarios1.getMedia());
 
 
 
