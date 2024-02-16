30e667af01fc6f46aecbaaeec707fdb195a5dfe3
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-16 19:36:08 -0300

    blocos de inicialização

---
+++ src/DevDojoMaratone/Javacore/Eblocosdeinicializacao/teste/AnimeTeste01.java
@@ -0,0 +1,12 @@
+package DevDojoMaratone.Javacore.Eblocosdeinicializacao.teste;
+
+import DevDojoMaratone.Javacore.Eblocosdeinicializacao.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime("one piece");
+        System.out.print(anime1.getEpisodios());
+
+
+    }
+}
