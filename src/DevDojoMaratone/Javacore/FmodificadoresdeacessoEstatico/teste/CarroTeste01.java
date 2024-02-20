8e6aa7e275fe791129c00b3ed7fe65cbd08698b4
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-20 20:44:07 -0300

    Podemos o val executar e exibir or da variavel de classe statica pois  ela nÃão está sendo executada pela instancia mas sim pÃpela propria classe

--- src/DevDojoMaratone/Javacore/FmodificadoresdeacessoEstatico/teste/CarroTeste01.java
+++ src/DevDojoMaratone/Javacore/FmodificadoresdeacessoEstatico/teste/CarroTeste01.java
@@ -6,21 +6,26 @@ public class CarroTeste01 {
     public static void main(String[] args) {
 
 
-        Carro c1 = new Carro("Bmw m5",280);
-        Carro c2 = new Carro("A M G Mercerdes",300);
-        Carro c3 = new Carro("Toyota Supra ",220);
+        //Carro c1 = new Carro("Bmw m5",280);
+        //Carro c2 = new Carro("A M G Mercerdes",300);
+        //Carro c3 = new Carro("Toyota Supra ",220);
 
         //agora podemos setar o valor da velocidade limite utilizando a propria classe, pois é um metadado da Classe
         // todas as instancia de Carro vão ter o valor associado ao limite...
         // não utiliza a variavel de referencia para alterar apesar de funcionar
 
+
+        // podemos exibir o valor somente da variavel de classe pois agora ele é independente da instancia pois pertence a classe e não ao objeto.
         Carro.velocidadeLimite = 120;
+        System.out.println(Carro.velocidadeLimite);
+        Carro.velocidadeLimite = 100;
+        System.out.println(Carro.velocidadeLimite);
 
-        c1.imprime();
+        //c1.imprime();
 
-        c2.imprime();
+        //c2.imprime();
 
-        c3.imprime();
+        //c3.imprime();
 
     }
 }
