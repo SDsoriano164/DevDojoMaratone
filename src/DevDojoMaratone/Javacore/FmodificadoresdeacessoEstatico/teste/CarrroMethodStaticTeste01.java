cad7451a43de2ce4cef81639ab1d46e9cf44dd73
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-22 20:37:34 -0300

    Podemos mostrar o valor setado para a variavel de classe, pois independe da instancia da classe

---
+++ src/DevDojoMaratone/Javacore/FmodificadoresdeacessoEstatico/teste/CarrroMethodStaticTeste01.java
@@ -0,0 +1,21 @@
+package DevDojoMaratone.Javacore.FmodificadoresdeacessoEstatico.teste;
+
+
+import DevDojoMaratone.Javacore.FmodificadoresdeacessoEstatico.dominio.CarroMethodStatic;
+
+public class CarrroMethodStaticTeste01 {
+    public static void main(String[] args) {
+        CarroMethodStatic.setVelocidadeLimite(150);
+        System.out.println(CarroMethodStatic.getVelocidadeLimite());
+
+
+       // CarroMethodStatic car1 = new CarroMethodStatic("M A Z D A - R X 8 ",231);
+      //  CarroMethodStatic car2 = new CarroMethodStatic("W O L K W A G E M  - J E T T A ",231);
+
+
+        //car1.imprime();
+        //car2.imprime();
+
+
+    }
+}
