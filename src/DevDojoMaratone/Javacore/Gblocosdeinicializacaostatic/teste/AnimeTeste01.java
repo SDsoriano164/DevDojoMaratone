77013f8c0778af6147a1a26ab23234fa57fcf44c
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-23 22:10:27 -0300

    não é necessario  criar um objeto para acessar e mostrar o membro da classe, pois ele é independente

---
+++ src/DevDojoMaratone/Javacore/Gblocosdeinicializacaostatic/teste/AnimeTeste01.java
@@ -0,0 +1,16 @@
+package DevDojoMaratone.Javacore.Gblocosdeinicializacaostatic.teste;
+
+import DevDojoMaratone.Javacore.Gblocosdeinicializacaostatic.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+
+
+      Anime.imprime();
+
+
+
+
+
+    }
+}
