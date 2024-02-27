6e811179814b6d32605ac84ed4f8292880354e0e
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-26 22:27:27 -0300

    Classe associada a classe escola argumentos de criação de objeto passados na cinstancia do objeto escola

---
+++ src/DevDojoMaratone/Javacore/HAssociacoes/domain/Professor2.java
@@ -0,0 +1,18 @@
+package DevDojoMaratone.Javacore.HAssociacoes.domain;
+
+public class Professor2 {
+    private String nomeProfessor;
+
+
+    public Professor2(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+    }
+}
