b1e7442f659fbce5e18c52bcdb110e89ec62848d
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-02-28 21:33:25 -0300

    Classe Local

---
+++ src/DevDojoMaratone/Javacore/HAssociacoes/Exercicio/Local.java
@@ -0,0 +1,17 @@
+package DevDojoMaratone.Javacore.HAssociacoes.Exercicio;
+
+public class Local {
+    private String endereço;
+
+    public Local(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+}
