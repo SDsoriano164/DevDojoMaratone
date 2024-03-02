d6b57b49e2f1bf1e25f930292f8f211ee7c2ffad
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-01 21:11:39 -0300

    Refactor class Estudante

--- src/DevDojoMaratone/Javacore/HAssociacoes/Exercicio/Aluno.java
+++ src/DevDojoMaratone/Javacore/HAssociacoes/Exercicio/Aluno.java
@@ -1,18 +1,16 @@
 package DevDojoMaratone.Javacore.HAssociacoes.Exercicio;
 
-public class Estudante {
+public class Aluno {
     private String nome;
     private int idade;
     private Seminario seminario;
 
-
-    public Estudante(String nome, int idade ){
+    public Aluno(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
-
     }
 
-    public Estudante(String nome, int idade, Seminario seminario) {
+    public Aluno(String nome, int idade, Seminario seminario) {
         this.nome = nome;
         this.idade = idade;
         this.seminario = seminario;
@@ -32,5 +30,13 @@ public class Estudante {
 
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public Seminario getSeminario() {
+        return seminario;
+    }
+
+    public void setSeminario(Seminario seminario) {
+        this.seminario = seminario;
     }
 }
