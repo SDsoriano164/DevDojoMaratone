6ef28155f807f5431e81c36ca7624a07d327767e
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-06 21:07:19 -0300

    Criando a extensão de Pessoa utilizaremos os dados da classe Pessoa, para usar em Funcionarios, porque na lógica de Herança, herdaremos os atributos e funções definidas para uma classe em outra na lógica, Funcionário ÃÉ uma pessoa, por isso podemos herdar os campos de Pessoa.

---
+++ src/DevDojoMaratone/Javacore/iHerança/Dominio/Funcionario.java
@@ -0,0 +1,13 @@
+package DevDojoMaratone.Javacore.iHerança.Dominio;
+
+public class Funcionario extends Pessoa {
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
