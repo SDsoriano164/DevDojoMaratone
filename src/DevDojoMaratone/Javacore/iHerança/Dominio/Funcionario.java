cbdce967c604cbc3f2c58ebd3964fba65ca02c70
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-11 19:47:04 -0300

    Criando metodo de pagamento estamos imprimindo o this.getnome, pois só temos acesso ao atributo em tempo de execucao, o atributo sendo privado, não temos acesso diretamente ao atributo definido na classe

--- src/DevDojoMaratone/Javacore/iHerança/Dominio/Funcionario.java
+++ src/DevDojoMaratone/Javacore/iHerança/Dominio/Funcionario.java
@@ -7,6 +7,16 @@ public class Funcionario extends Pessoa {
         return salario;
     }
 
+    @Override
+    public void imprime() {
+        super.imprime();
+        System.out.println(" salario : " +this.salario);
+    }
+    public void relatorioPagamento(){
+        System.out.println(" Eu " +
+                " " + this.getNome() + " recebi o salario de " + this.salario);
+    }
+
     public void setSalario(double salario) {
         this.salario = salario;
     }
