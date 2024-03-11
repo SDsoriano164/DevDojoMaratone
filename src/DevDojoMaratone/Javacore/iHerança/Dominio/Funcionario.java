ce14b0636a07d2f3b88fa8f765cb78ada567643a
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-11 20:39:08 -0300

    Criando override para a superClasse na subclasse Se é criado um construtor para passar argumentos em uma classe, é necessário que a subclasse possua a mesma criação para que possa ser executado para que possamos Criar um construtor sobrescrito precisamos usar o super, que assim acessaremos o construtor para ser execitado pelo objeto

--- src/DevDojoMaratone/Javacore/iHerança/Dominio/Funcionario.java
+++ src/DevDojoMaratone/Javacore/iHerança/Dominio/Funcionario.java
@@ -7,6 +7,10 @@ public class Funcionario extends Pessoa {
         return salario;
     }
 
+    public Funcionario(String nome){
+        super(nome);
+    }
+
     @Override
     public void imprime() {
         super.imprime();
