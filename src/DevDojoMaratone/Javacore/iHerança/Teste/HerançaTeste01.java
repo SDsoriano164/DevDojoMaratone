97b3cb21d0d4afa78cfa9d024b43680b87b69399
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-11 19:47:59 -0300

    Criando teste de herança e imprimindo metodo pagamento

--- src/DevDojoMaratone/Javacore/iHerança/Teste/HerançaTeste01.java
+++ src/DevDojoMaratone/Javacore/iHerança/Teste/HerançaTeste01.java
@@ -6,27 +6,27 @@ import DevDojoMaratone.Javacore.iHerança.Dominio.Pessoa;
 
 public class HerançaTeste01 {
     public static void main(String[] args) {
-        Endereco endereco = new Endereco();
-        endereco.setRua("Rua 3");
-        endereco.setCep("312312");
+        Endereco endereco  = new Endereco();
+        endereco.setCepRua("58416-034");
+        endereco.setNomerua("Dev Street");
 
         Pessoa pessoa = new Pessoa();
-        pessoa.setNome("Toyohisa shimazu");
-        pessoa.setCpf("323231313");
-
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("Jiraya Sensei");
-        funcionario.setCpf("4445552222");
-        funcionario.setEndereco(endereco);
-
-
+        pessoa.setNome("Kilian mbape");
+        pessoa.setCpf("708.341.344-22");
 
         pessoa.setEndereco(endereco);
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Neymar Jr");
+        funcionario.setCpf("453.178.287-91");
+        funcionario.setSalario(2312);
+        funcionario.setEndereco(endereco);
 
         pessoa.imprime();
-        System.out.println("---------------------");
-        System.out.println("Funcinario");
+        System.out.println("-----------------");
+        System.out.println("=== funcionario ===");
+
         funcionario.imprime();
+        funcionario.relatorioPagamento();
 
     }
 
