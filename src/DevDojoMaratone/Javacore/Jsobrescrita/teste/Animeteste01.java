1fa68e2968cd04876a5b1cc4c836d9c62d9e46de
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-14 20:14:39 -0300

    Istânciando e imprimindo objeto

---
+++ src/DevDojoMaratone/Javacore/Jsobrescrita/teste/Animeteste01.java
@@ -0,0 +1,12 @@
+package DevDojoMaratone.Javacore.Jsobrescrita.teste;
+
+import DevDojoMaratone.Javacore.Jsobrescrita.dominio.Anime;
+
+public class Animeteste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr Stone");
+
+        System.out.println(anime);
+
+    }
+}
