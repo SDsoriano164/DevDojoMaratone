f11196b7f6ebc92743f70e71e1a2508b90b7aec2
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-25 21:27:49 -0300

    Criando classe teste classes abstratas

--- src/DevDojoMaratone/Javacore/MClasesAbstratas/teste/FuncionarioTeste01.java
+++ src/DevDojoMaratone/Javacore/MClasesAbstratas/teste/FuncionarioTeste01.java
@@ -1,2 +1,18 @@
-package DevDojoMaratone.Javacore.MClasesAbstratas.teste;public class FuncionarioTeste01 {
+package DevDojoMaratone.Javacore.MClasesAbstratas.teste;
+
+import DevDojoMaratone.Javacore.MClasesAbstratas.dominio.Desenvolvedor;
+import DevDojoMaratone.Javacore.MClasesAbstratas.dominio.Funcionario;
+import DevDojoMaratone.Javacore.MClasesAbstratas.dominio.Gerente;
+
+public class FuncionarioTeste01 {
+    public static void main(String[] args) {
+       // Funcionario funcionario = new Funcionario("Zoro",2000);  não pode ser istânciado pois a classe funcionario é abstrato ( é só um template );
+
+        Gerente gerente = new Gerente("Luffy",5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Myamoto",5000);
+
+
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
 }
