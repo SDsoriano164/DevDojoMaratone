9c19955d123225cdc84c0f065c687c19af3157da
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-25 21:26:53 -0300

    Criando classes abstratas criando classes abstratas a partir de uma superclass, e aprendendo a utilizar metodos abstratos

---
+++ src/DevDojoMaratone/Javacore/MClasesAbstratas/dominio/Gerente.java
@@ -0,0 +1,23 @@
+package DevDojoMaratone.Javacore.MClasesAbstratas.dominio;
+
+public final class Gerente extends Funcionario{
+
+    public Gerente(String nome, double salario) {
+        super(nome, salario);
+    }
+
+    @Override
+    public void calculaBonus() {
+        this.salario = salario + salario * 0.1;
+    }
+
+    @Override
+    public String toString() {
+        return "Gerente{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+
+
+}
