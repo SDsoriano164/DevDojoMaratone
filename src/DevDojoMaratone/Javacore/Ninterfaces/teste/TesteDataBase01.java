250ad20873ade874c3866eed3dd661b30bc1b06a
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-27 08:02:37 -0300

    Classe teste com metodos static e atributos static Atributos e metodos da interface

--- src/DevDojoMaratone/Javacore/Ninterfaces/teste/TesteDataBase01.java
+++ src/DevDojoMaratone/Javacore/Ninterfaces/teste/TesteDataBase01.java
@@ -1,21 +1,38 @@
 package DevDojoMaratone.Javacore.Ninterfaces.teste;
 
 import DevDojoMaratone.Javacore.Ninterfaces.dominio.DataBaseLoader;
+import DevDojoMaratone.Javacore.Ninterfaces.dominio.DataLoader;
 import DevDojoMaratone.Javacore.Ninterfaces.dominio.FileLoader;
 
 public class TesteDataBase01 {
     public static void main(String[] args) {
+        // instânciando Classes
         DataBaseLoader dataBaseLoader = new DataBaseLoader();
         FileLoader fileLoader = new FileLoader();
 
+        // chamada para  o metodo da interface implementada na classe
         fileLoader.load();
         dataBaseLoader.load();
-
         dataBaseLoader.remove();
         fileLoader.remove();
 
-        // classe default definida na interface Dataloader
+        // classes default definida na interface Dataloader // chamada para o metodo
         dataBaseLoader.checkingPermission();
         fileLoader.checkingPermission();
+        dataBaseLoader.verivy();
+
+
+        // acessando atributo da interface ( são constantes por padrão )
+        dataBaseLoader.imprime();
+
+
+        // lembre-se membro estaticos acessamos diretamente pela Classe e não pela referencia
+
+        // metodo static dentro da classe DataBaseLoader
+        DataBaseLoader.retriveMaxDataSize();
+
+        //  metodo static dentro da interface dataLoader
+        DataLoader.retriveMaxDataSize();
+
     }
 }
