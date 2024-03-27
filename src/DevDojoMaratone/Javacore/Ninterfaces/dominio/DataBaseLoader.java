af58e4f0f79babf84856f9f5620ea06a3b4dee5c
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-27 07:58:29 -0300

    informações importantes não conseguimos sobrescrever metodos staticos da interface.

--- src/DevDojoMaratone/Javacore/Ninterfaces/dominio/DataBaseLoader.java
+++ src/DevDojoMaratone/Javacore/Ninterfaces/dominio/DataBaseLoader.java
@@ -1,6 +1,11 @@
 package DevDojoMaratone.Javacore.Ninterfaces.dominio;
 
 public class DataBaseLoader implements DataLoader, DataRemove {
+
+    public void imprime(){
+        System.out.println(MAX_DATA_SIZE);
+
+    }
     @Override
     public void load() {
         System.out.println("Loading DataBase data...");
@@ -15,5 +20,16 @@ public class DataBaseLoader implements DataLoader, DataRemove {
     @Override
     public void checkingPermission() {
         System.out.println("Checking permission ");
+    }
+
+    @Override
+    public void verivy() {
+        System.out.println("Verify files...");
+    }
+
+    // não conseguimos sobrecrever metodos estatico da interface, podemos usar o mesmo nome porém acessamos ele de forma diferente
+    // PARA A CLASSE E PARA A INTERFACE
+    public static void retriveMaxDataSize(){ // executado na classe
+        System.out.println("Dentro da interface DataLoader");
     }
 }
