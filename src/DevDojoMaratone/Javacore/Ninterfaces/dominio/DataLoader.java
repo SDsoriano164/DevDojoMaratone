6db07fee8c778e9005a570dd2602dbba9f45edd9
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-27 08:00:44 -0300

    Classe static e metodo static criado atributos em interfaces sempre sao constantes, podem ser acessado diretamente da interface metodos static nao podem ser sobrescritos, e podem ser acessados na interface

--- src/DevDojoMaratone/Javacore/Ninterfaces/dominio/DataLoader.java
+++ src/DevDojoMaratone/Javacore/Ninterfaces/dominio/DataLoader.java
@@ -1,9 +1,14 @@
 package DevDojoMaratone.Javacore.Ninterfaces.dominio;
 
 public interface DataLoader {
+    public  static final int MAX_DATA_SIZE = 10; //TODO ATRIBUTO EM INTERFACE SAO CONSTANTES.
     public abstract void load();
     public default void checkingPermission(){ // Criando metodo para ser implementado na classe.
-
+    }
+    default  void verivy (){
+    }
+    public static void retriveMaxDataSize(){
+        System.out.println("Dentro da interface DataLoader");
     }
 
 }
