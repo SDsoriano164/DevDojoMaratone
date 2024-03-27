0cbd3e4b570daf1a7d316ee4bae93981e027a76a
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-26 22:11:55 -0300

    Interface Criada, DataLoader Interfaces são classes que por padrÃrao sao abstratas e servem para serem implementadas 'implements' em outras classes, em que a classe implementada, tem que sobrescrever o metodo obrigatoriamente

---
+++ src/DevDojoMaratone/Javacore/Ninterfaces/dominio/DataLoader.java
@@ -0,0 +1,9 @@
+package DevDojoMaratone.Javacore.Ninterfaces.dominio;
+
+public interface DataLoader {
+    public abstract void load();
+    public default void checkingPermission(){ // Criando metodo para ser implementado na classe.
+
+    }
+
+}
