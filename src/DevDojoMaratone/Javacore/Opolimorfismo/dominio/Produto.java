224958f53405a90c461c310fb6b6db58fd00704b
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-27 21:42:00 -0300

    Classe abstrata com metodo implementado

---
+++ src/DevDojoMaratone/Javacore/Opolimorfismo/dominio/Produto.java
@@ -0,0 +1,27 @@
+package DevDojoMaratone.Javacore.Opolimorfismo.dominio;
+
+public abstract class Produto implements Taxavel {
+    protected String nome;
+    protected int valor;
+
+    public Produto(String nome, int valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
