0002f94d3e6f3f0f167419299e70298d93bb7099
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-28 20:54:03 -0300

    Alteracao alteracao para passar argumento produto quando um produto for intanciado como um novo computador

--- src/DevDojoMaratone/Javacore/Opolimorfismo/servicos/CalculadoraImposto.java
+++ src/DevDojoMaratone/Javacore/Opolimorfismo/servicos/CalculadoraImposto.java
@@ -1,6 +1,7 @@
 package DevDojoMaratone.Javacore.Opolimorfismo.servicos;
 
 import DevDojoMaratone.Javacore.Opolimorfismo.dominio.Computador;
+import DevDojoMaratone.Javacore.Opolimorfismo.dominio.Produto;
 import DevDojoMaratone.Javacore.Opolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
@@ -13,12 +14,22 @@ public class CalculadoraImposto {
         double valorcomimpostos = computador.getValor() + imposto;
         System.out.println( "Valor com impostos : " + valorcomimpostos);
     }
-    public  void calcularImpostoTomate(Tomate tomate) {
+    public  static void calcularImpostoTomate(Tomate tomate) {
         System.out.println("Relatorio de imposto do tomate ");
         double imposto = tomate.calcularImposto();
         System.out.println("nome do tomate : " + tomate.getNome());
         System.out.println("valor to domate :" + tomate.getNome());
         System.out.println("imposto a ser pago :" + imposto);
+    }
+    public static  void calcularImpostoProduto (Produto produto){
+        System.out.println("Relatorio de imposto do computador ;");
+        double imposto = produto.calcularImposto();
+        System.out.println("Nome do computador:" + produto.getNome());
+        System.out.println("Imposto do computador: " + produto.getValor());
+        System.out.println("Imposto a ser pago : " + imposto);
+        double valorcomimpostos = produto.getValor() + imposto;
+        System.out.println( "Valor com impostos : " + valorcomimpostos);
+
     }
 
 }
