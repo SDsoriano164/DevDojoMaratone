fd840ad5f675c7be81f46cbad2cfa31ed35f1e6e
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-30 12:17:14 -0300

    Criando novo atributo

--- src/DevDojoMaratone/Javacore/Opolimorfismo/dominio/Tomate.java
+++ src/DevDojoMaratone/Javacore/Opolimorfismo/dominio/Tomate.java
@@ -2,6 +2,7 @@ package DevDojoMaratone.Javacore.Opolimorfismo.dominio;
 
 public class Tomate extends Produto{
     public static final double IMPOSTO_TOMATE = 0.06;
+    private String dataValidade;
 
     public Tomate(String nome, int valor) {
         super(nome, valor);
@@ -11,5 +12,13 @@ public class Tomate extends Produto{
     public double calcularImposto() {
         System.out.println("Calculando imposto do tomate ...");
         return this.valor * IMPOSTO_TOMATE ;
+    }
+
+    public void setDataValidade(String  dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
     }
 }
