bc67e201cce2616d9c253577c512686c270cc5cd
Pedro Soriano <p3dro.hs.silva@gmail.com>
2024-03-30 12:12:11 -0300

    Casting e istanceof para que a gente possa a istanceof

--- src/DevDojoMaratone/Javacore/Opolimorfismo/servicos/CalculadoraImposto.java
+++ src/DevDojoMaratone/Javacore/Opolimorfismo/servicos/CalculadoraImposto.java
@@ -5,30 +5,24 @@ import DevDojoMaratone.Javacore.Opolimorfismo.dominio.Produto;
 import DevDojoMaratone.Javacore.Opolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
-    public static void calcularImpostoComputador (Computador computador){
-        System.out.println("Relatorio de imposto do computador ;");
-        double imposto = computador.calcularImposto();
-        System.out.println("Nome do computador:" + computador.getNome());
-        System.out.println("Imposto do computador: " + computador.getValor());
-        System.out.println("Imposto a ser pago : " + imposto);
-        double valorcomimpostos = computador.getValor() + imposto;
-        System.out.println( "Valor com impostos : " + valorcomimpostos);
-    }
-    public  static void calcularImpostoTomate(Tomate tomate) {
-        System.out.println("Relatorio de imposto do tomate ");
-        double imposto = tomate.calcularImposto();
-        System.out.println("nome do tomate : " + tomate.getNome());
-        System.out.println("valor to domate :" + tomate.getNome());
-        System.out.println("imposto a ser pago :" + imposto);
-    }
+
     public static  void calcularImpostoProduto (Produto produto){
         System.out.println("Relatorio de imposto do computador ;");
         double imposto = produto.calcularImposto();
-        System.out.println("Nome do computador:" + produto.getNome());
-        System.out.println("Imposto do computador: " + produto.getValor());
+        System.out.println("Nome do produto" + produto.getNome());
+        System.out.println("Imposto do produto: " + produto.getValor());
         System.out.println("Imposto a ser pago : " + imposto);
         double valorcomimpostos = produto.getValor() + imposto;
         System.out.println( "Valor com impostos : " + valorcomimpostos);
+        System.out.println("--------------------------------------");
+
+        // verificando se o produto é uma istancia de toamate
+        if (produto instanceof Tomate){
+            Tomate tomate  = (Tomate) produto;
+            System.out.println(tomate.getDataValidade());
+
+
+        }
 
     }
 
